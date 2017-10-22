d2f7283ab099748c7f8b14000ba45deab5d90e60
DESKTOP-SES54OO\wsong <wsong0101@gmail.com>
2017-10-22 22:03:29 +0900

    onRequiredGameBoard handler create(ing)

--- Assets/Scripts/Server/GameServer.cs
+++ Assets/Scripts/Server/GameServer.cs
@@ -23,6 +23,7 @@ namespace WSTichu.Server
 			Action installMessageHandlers = () =>
 			{
 				_network.RegisterHandler(MessageType.Connect, onConnected);
+				_network.RegisterHandler(MessageType.CS_RequireGameBoard, onRequireGameBoard);
 			};
 
 			setupServerConfig();
@@ -40,9 +41,38 @@ namespace WSTichu.Server
 			_network.Update();
 		}
 
+		//-- packet handler defines
 		private void onConnected(NetworkMessage source)
 		{
 			Debug.LogFormat("[{0}] New client connected : {1}", nameof(GameServer), source.conn);
+		}
+
+		private void onRequireGameBoard(NetworkMessage netMsg)
+		{
+			SC_GameBoardDump dump = new SC_GameBoardDump();
+			GameBoard board = new GameBoard();
+
+			List<Card> cards = new List<Card>
+			{
+				Card.Unknown,
+				Card.Unknown,
+				Card.Unknown,
+				Card.Unknown
+			};
+
+			board.LastCombination = Combination.Factory.CreateFromCards(cards);
+
+
+			Player p1 = new Player();
+			Dictionary<TeamType, Player[]> players = new Dictionary<TeamType, Player[]>();
+			Player[] plys = { p1, p1 };
+			players.Add(TeamType.kTeam1, plys);
+
+			board.Players = players;
+
+			dump.CurrentGameBoard = board;
+
+			netMsg.conn.Send(MessageType.SC_GameBoardDump, dump);
 		}
 	}
 }
